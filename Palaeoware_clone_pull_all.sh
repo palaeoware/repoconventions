75c541028fb65f1c65175dd88d1b771a3d4c3586
Russell Garwood <russell.garwood@manchester.ac.uk>
2018-10-26 10:56:14 +0100

    Added a bash script to clone or pull all repos in Palaeoware organisation

---
+++ Palaeoware_clone_pull_all.sh
@@ -0,0 +1,68 @@
+#This is a shell script which either clones or pulls the palaeoware repositories
+
+#!/bin/bash
+
+echo "Cloning/pulling RevoSim"
+if [[ ! -d "envirogen" ]]; then
+git clone https://github.com/palaeoware/envirogen.git
+else 
+cd "envirogen"
+git pull
+cd ..
+fi
+
+
+echo "Cloning/pulling REvoSim"
+if [[ ! -d "revosim" ]]; then
+git clone https://github.com/palaeoware/revosim.git
+else 
+cd "revosim"
+git pull
+cd ..
+fi
+
+echo "Cloning/pulling TREvoSim"
+if [[ ! -d "trevosim" ]]; then
+git clone https://github.com/palaeoware/trevosim.git
+else 
+cd "trevosim"
+git pull
+cd ..
+fi
+
+echo "Cloning/pulling MBL2017"
+if [[ ! -d "MBL2017" ]]; then
+git clone https://github.com/palaeoware/MBL2017.git
+else 
+cd "MBL2017"
+git pull
+cd ..
+fi
+
+echo "Cloning/pulling SPIERS"
+if [[ ! -d "SPIERS" ]]; then
+git clone https://github.com/palaeoware/SPIERS.git
+else 
+cd "SPIERS"
+git pull
+cd ..
+fi
+
+echo "Cloning/pulling SPIERSwebsite"
+if [[ ! -d "SPIERSwebsite" ]]; then
+git clone https://github.com/palaeoware/SPIERSwebsite.git
+else 
+cd "SPIERSwebsite"
+git pull
+cd ..
+fi
+
+echo "Cloning/pulling repoconventions"
+if [[ ! -d "repoconventions" ]]; then
+git clone https://github.com/palaeoware/repoconventions.git
+else 
+cd "repoconventions"
+git pull
+cd ..
+fi
+
